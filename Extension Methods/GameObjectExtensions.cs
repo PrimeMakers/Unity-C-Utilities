1b2c33cb26493e6e64960849ac94a40888523031
Ricardo Pires <ricardo.pires@primemakers.com>
2017-03-07 12:41:57 +0000

    Update GameObjectExtensions.cs

--- Extension Methods/GameObjectExtensions.cs
+++ Extension Methods/GameObjectExtensions.cs
@@ -2,5 +2,12 @@ using UnityEngine;
 
 public static class GameObjectExtensions
 {
-
+	// This method is used so you can find a child in a faster way and convert it directly to a GameObject
+	public static GameObject FindChild(this GameObject obj, string name)
+	{
+		return obj.tranform.FindChild(name).gameObject;
+	}
+	
+	// This method asdasd
+	public static GameObject FindParent
 }
